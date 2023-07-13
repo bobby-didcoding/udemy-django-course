0c1b1ef3d3b43b00e46ecdc36edc059fe4b7446c
bobby-oddersea <bobby@oddersea.com>
2023-07-13 16:50:00 +0100

    fixed login url

--- app/backend/users/urls.py
+++ app/backend/users/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 	##### Generic user views ######
 	path('signup/',views.SignUpView.as_view(),name="signup"),
 	path('activate/<uidb64>/<token>/',views.activate, name='activate'),
-	path('login/',views.login_user,name="parent-login"),
+	path('login/',views.login_user,name="login"),
 	path('logout/',views.logout,name="logout"),
 	path('forgotten-password/',views.forgotten_password,name="forgotten-password"),
    
