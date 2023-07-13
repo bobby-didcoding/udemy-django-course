1d3c829cd3eeeca638e1adf269e448694e89975d
bobby-oddersea <bobby@oddersea.com>
2023-07-13 14:33:31 +0100

    all app sitemaps added

--- app/backend/ecommerce/views/__init__.py
+++ app/backend/ecommerce/views/__init__.py
@@ -4,9 +4,13 @@
 from ecommerce.views.cart import CartView
 from ecommerce.views.session_success import SessionSuccessView
 from ecommerce.views.session_cancelled import SessionCancelledView
+from ecommerce.views.product import ProductView
+from ecommerce.views.products import ProductsView
 
 __all__ = [
     CartView,
     SessionSuccessView,
     SessionCancelledView,
+    ProductsView,
+    ProductsView
 ]
---
+++ app/backend/users/sitemaps.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------
+# Django imports
+# --------------------------------------------------------------
+from django.contrib import sitemaps
+from django.urls import reverse
+
+
+class UsersStaticViewsSitemap(sitemaps.Sitemap):
+
+	priority = 0.5
+	changefreq = "daily"
+
+	def items(self):
+
+		return [
+			'users:login',
+			'users:signup'
+		]
+
+	def location(self, item):
+		return reverse(item)
