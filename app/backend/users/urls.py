8ddc90ae7b78d0080afdbec81e8f8fd615f5bc4d
bobby-oddersea <bobby@oddersea.com>
2023-07-17 16:33:21 +0100

    fixed logout bug

--- app/backend/users/urls.py
+++ app/backend/users/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
 	path('signup/',views.SignUpView.as_view(),name="signup"),
 	path('activate/<uidb64>/<token>/',views.activate, name='activate'),
 	path('login/',views.login_user,name="login"),
-	path('logout/',views.logout,name="logout"),
+	path('logout/',views.logout_user,name="logout"),
 	path('forgotten-password/',views.forgotten_password,name="forgotten-password"),
    
 ]
