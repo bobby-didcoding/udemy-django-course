71d935c7fc914204318d5a5d5d93ba05aa2c0f2e
bobby-oddersea <bobby@oddersea.com>
2023-07-17 16:29:58 +0100

    fixed logout bug

--- app/backend/users/views/__init__.py
+++ app/backend/users/views/__init__.py
@@ -2,14 +2,14 @@
 # App imports
 # --------------------------------------------------------------
 from users.views.signup import SignUpView
-from users.views.signout import logout
+from users.views.signout import logout_user
 from users.views.login import login_user
 from users.views.activate import activate
 from users.views.forgotten_password import forgotten_password
 
 __all__ = [
     SignUpView,
-    logout,
+    logout_user,
     login_user,
     activate,
     forgotten_password
