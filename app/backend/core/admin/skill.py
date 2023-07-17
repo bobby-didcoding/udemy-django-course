e0cb9e45fe1b19472dbad427ed58d72688df5763
bobby-oddersea <bobby@oddersea.com>
2023-07-17 14:10:36 +0100

    added admin

---
+++ app/backend/core/admin/skill.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------
+# Django imports
+# --------------------------------------------------------------
+from django.contrib import admin
+from core.models import Skill
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
