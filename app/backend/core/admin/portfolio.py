29a4634c477e67c918f761904e6685f5bea5dbd1
bobby-oddersea <bobby@oddersea.com>
2023-07-17 14:06:58 +0100

    added admin

---
+++ app/backend/core/admin/portfolio.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------
+# Django imports
+# --------------------------------------------------------------
+from django.contrib import admin
+from core.models import Portfolio
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
