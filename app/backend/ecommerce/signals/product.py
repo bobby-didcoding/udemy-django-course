cef0d3a3e21100c44e777931d3158a372ace3a2c
bobby-oddersea <bobby@oddersea.com>
2023-07-17 19:49:44 +0100

    signals added

---
+++ app/backend/ecommerce/signals/product.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------------
+# Python imports
+# --------------------------------------------------------------
+import logging
+
+# --------------------------------------------------------------
+# Django imports
+# --------------------------------------------------------------
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+# --------------------------------------------------------------
+# App imports
+# --------------------------------------------------------------
+from ecommerce.models import Product
+
+logger = logging.getLogger(__name__)
+
+@receiver(post_save, sender=Product, weak=False)
+def create_product(sender, instance, created, **kwargs):
+    if created:
+        '''
+        Create a stripe product
+        '''
+        pass
