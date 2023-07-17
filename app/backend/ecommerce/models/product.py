e7cc8005fb60a53b1afbf4c8ef8f38e00f2bff54
bobby-oddersea <bobby@oddersea.com>
2023-07-17 13:40:06 +0100

    added product image

--- app/backend/ecommerce/models/product.py
+++ app/backend/ecommerce/models/product.py
@@ -1,4 +1,9 @@
 # --------------------------------------------------------------
+# Python imports
+# --------------------------------------------------------------
+import os
+
+# --------------------------------------------------------------
 # Django imports
 # --------------------------------------------------------------
 from django.utils.translation import gettext_lazy as _
@@ -22,12 +27,20 @@ from ecommerce.models import Price
 from django_extensions.db.models import TitleSlugDescriptionModel
 
 
+def product_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/products/<slug>/<filename>
+    return os.path.join('products', str(instance.slug), filename)
+
 class Product(
     TitleSlugDescriptionModel,
     ExternalID,
     Model):
 
     price = models.ForeignKey(Price, blank=True, null=True, on_delete=models.SET_NULL, related_name="product_price")
+    image = models.ImageField(_('image'),upload_to=product_directory_path, default="default_image.jpg")
 
     def __Str__(self):
         return f'{self.title}'
+    
+    def get_absolute_url(self):
+        return f'/product/{self.slug}/'
