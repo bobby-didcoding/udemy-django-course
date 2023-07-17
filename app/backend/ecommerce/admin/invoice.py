46ccef87d542050f648e762dff6752b821f995d0
bobby-oddersea <bobby@oddersea.com>
2023-07-17 14:04:15 +0100

    added admin

---
+++ app/backend/ecommerce/admin/invoice.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------
+# Django imports
+# --------------------------------------------------------------
+from django.contrib import admin
+
+# --------------------------------------------------------------
+# App imports
+# --------------------------------------------------------------
+from ecommerce.models import Invoice
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'external_id', 'customer')
