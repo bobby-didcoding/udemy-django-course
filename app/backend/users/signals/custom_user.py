669a7edace84c5efd337d72465ef5531c12b289a
bobby-oddersea <bobby@oddersea.com>
2023-07-18 15:33:14 +0100

    added tests

--- app/backend/users/signals/custom_user.py
+++ app/backend/users/signals/custom_user.py
@@ -1,20 +1,25 @@
 # --------------------------------------------------------------
 # Django imports
 # --------------------------------------------------------------
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.db.models.signals import pre_save
 from django.contrib.auth import get_user_model
 
 # --------------------------------------------------------------
 # Project imports
 # --------------------------------------------------------------
 from ecommerce.models import Customer, Cart
+from utils.decorators import suspendingreceiver
 
 
 User = get_user_model()
 
-@receiver(post_save, sender=User, weak=False)
-def create_ecommerce_objects(sender, instance, created, **kwargs):
-    if created:
-        Customer.objects.create(user = instance)
-        Cart.objects.create(user = instance)
+@suspendingreceiver(pre_save, sender=User, weak=False)
+def create_ecommerce_objects(sender, instance, **kwargs):
+    try:
+        current = instance
+        previous = sender.objects.get(id = instance.id)
+        if previous.is_active == False and current.is_active:
+            customer = Customer.objects.create(user = instance)
+            Cart.objects.create(customer = customer)
+    except sender.DoesNotExist:
+        pass
