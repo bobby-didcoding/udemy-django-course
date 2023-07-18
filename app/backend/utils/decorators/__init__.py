6879c8f2d535f9297c7878c523bd198ef69b11d3
bobby-oddersea <bobby@oddersea.com>
2023-07-18 15:25:14 +0100

    added tests

---
+++ app/backend/utils/decorators/__init__.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------
+# App imports
+# --------------------------------------------------------------
+from utils.decorators.suspend_receiver import suspendingreceiver
+
+
+__all__ = [
+    suspendingreceiver
+]
--- app/backend/utils/mixins/__init__.py
+++ app/backend/utils/mixins/__init__.py
@@ -2,6 +2,8 @@
 # Python imports
 # --------------------------------------------------------------
 import six
+import random
+import string
 
 # --------------------------------------------------------------
 # Django imports
@@ -16,7 +18,16 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 # --------------------------------------------------------------
 import requests
 
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
+def get_random_email(length):
+    random_string = f'{get_random_string(length)}.com'
+    half_length = int(round(length/2,0))
+    result_str = f'{random_string[:half_length]}@{random_string[half_length:]}'
+    return result_str
 
 def reCAPTCHAValidation(token):
 
