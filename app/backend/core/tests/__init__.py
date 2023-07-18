27e3f889f218c5f21dff44c9569c706519ef86d3
bobby-oddersea <bobby@oddersea.com>
2023-07-18 15:30:43 +0100

    tests added

---
+++ app/backend/core/tests/__init__.py
@@ -0,0 +1,32 @@
+# --------------------------------------------------------------
+# App imports
+# --------------------------------------------------------------
+from core.tests.models import (
+    BlogTestCase,
+    CertificateTestCase,
+    PolicyTestCase,
+    PortfolioTestCase,
+    ContactTestCase,
+    NewsLetterTestCase,
+    SkillTestCase,
+    TestimonialTestCase
+)
+
+from core.tests.forms import (
+    ContactFormTestCase,
+    GenericNewsLetterFormTestCase
+)
+
+
+__all__ = [
+    BlogTestCase,
+    CertificateTestCase,
+    PolicyTestCase,
+    PortfolioTestCase,
+    ContactTestCase,
+    NewsLetterTestCase,
+    SkillTestCase,
+    TestimonialTestCase,
+    ContactFormTestCase,
+    GenericNewsLetterFormTestCase
+]
---
+++ app/backend/ecommerce/tests/__init__.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------
+# App imports
+# --------------------------------------------------------------
+from ecommerce.tests.models import(
+    PriceTestCase,
+    ProductTestCase,
+    CustomerTestCase,
+    CartTestCase
+)
+
+
+__all__ = [
+
+    PriceTestCase,
+    ProductTestCase,
+    CustomerTestCase,
+    CartTestCase
+]
