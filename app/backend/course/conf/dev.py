ecd40c65e32ff7fb060081d0a9a53c3d41185ae2
bobby-oddersea <bobby@oddersea.com>
2023-07-19 11:02:20 +0100

    adjusting settings and envs

--- app/backend/course/conf/dev.py
+++ app/backend/course/conf/dev.py
@@ -5,31 +5,8 @@ import os
 import socket
 load_dotenv()
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 PRODUCTION = 0
 DEBUG = 1
-
-# Application definition
-RUN_SERVER_PORT = 8000
-
-# --------------------------------------------------------------
-# DATABASE SETTINGS
-# --------------------------------------------------------------
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("DB_USER", "user"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
-    }
-}
-# --------------------------------------------------------------
-# END DATABASE SETTINGS
-# --------------------------------------------------------------
-
 
 # --------------------------------------------------------------
 # STATICFILES SETTINGS
@@ -73,40 +50,4 @@ if DEBUG:
     INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + ['127.0.0.1', '10.0.2.2']
 # --------------------------------------------------------------
 # END DEBUG TOOLBAR SETTINGS
-# --------------------------------------------------------------
-
-# --------------------------------------------------------------
-# EMAIL SETTINGS
-# --------------------------------------------------------------
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-if EMAIL_USE_TLS:
-    EMAIL_USE_TLS = True
-else:
-    EMAIL_USE_TLS = False
-EMAIL_HOST_USER = os.environ.get("DONOT_REPLY_EMAIL")
-DISPLAY_NAME = "Udemy Course"
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-# --------------------------------------------------------------
-# END EMAIL SETTINGS
-# --------------------------------------------------------------
-
-
-# --------------------------------------------------------------
-# RECAPTCHA SETTINGS
-# --------------------------------------------------------------
-RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
-RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
-# --------------------------------------------------------------
-# END RECAPTCHA SETTINGS
-# --------------------------------------------------------------
-
-# --------------------------------------------------------------
-# COOKIE SETTINGS
-# --------------------------------------------------------------
-COOKIE_BOT = os.environ.get("COOKIE_BOT",None)
-# --------------------------------------------------------------
-# END COOKIE SETTINGS
 # --------------------------------------------------------------
