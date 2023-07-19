4ccb55c9e4dcdfcbad2df920a2917398fb5b9d93
bobby-oddersea <bobby@oddersea.com>
2023-07-19 11:05:44 +0100

    middleware added

--- app/backend/course/conf/common.py
+++ app/backend/course/conf/common.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.NewsLetterFormMiddleware', #needed to add newsletter form to each view
 ]
 
 ROOT_URLCONF = 'course.urls'
