eafdda03d331a1a168cd54010632101ccff23510
bobby stearman <bobby@didcoding.com>
2023-07-19 22:47:33 +0100

    removed invoiceitems

--- app/backend/ecommerce/signals/__init__.py
+++ app/backend/ecommerce/signals/__init__.py
@@ -5,10 +5,12 @@
 from ecommerce.signals.customer import create_customer
 from ecommerce.signals.price import create_price
 from ecommerce.signals.product import create_product
+from ecommerce.signals.invoice import create_invoice
 
 
 __all__ = [
     create_customer,
     create_price,
-    create_product
+    create_product,
+    create_invoice
 ]
