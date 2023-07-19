41ed81fe14d64666565b5893c93500d713d96862
bobby stearman <bobby@didcoding.com>
2023-07-19 22:50:39 +0100

    removed invoiceitems

--- app/backend/ecommerce/admin/__init__.py
+++ app/backend/ecommerce/admin/__init__.py
@@ -4,7 +4,6 @@
 
 from ecommerce.admin.cart import CartAdmin
 from ecommerce.admin.customer import CustomerAdmin
-from ecommerce.admin.invoice_item import InvoiceItemAdmin
 from ecommerce.admin.invoice import InvoiceAdmin
 from ecommerce.admin.price import PriceAdmin
 from ecommerce.admin.product import ProductAdmin
@@ -13,7 +12,6 @@ from ecommerce.admin.session_item import SessionItemAdmin
 
 __all__ = [
     CustomerAdmin,
-    InvoiceItemAdmin,
     InvoiceAdmin,
     PriceAdmin,
     ProductAdmin,
